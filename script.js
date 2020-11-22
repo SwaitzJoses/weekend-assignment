1c49ee1cdcb5252057ebbf1f777deac90a0f6245
SwaitzJoses <68174417+SwaitzJoses@users.noreply.github.com>
2020-11-23 01:41:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,389 @@
+var btn = document.createElement("div") ;
+btn.setAttribute("class", "bg" );
+
+
+var btn1 = document.createElement("img") ;
+btn1.setAttribute("src", "102419_disney-the-mandalorian-0.0-1-780x440-1572307750.jpg" );
+btn1.setAttribute("class","gray")
+document.body.appendChild(btn1);
+
+var btn2 = document.createElement("div")
+btn2.setAttribute("class", "bg2" );
+
+
+
+document.body.appendChild(btn);
+btn.appendChild(btn2); 
+
+var div3 = document.createElement("div") ;
+div3.setAttribute("class", "bg3" );
+btn.appendChild(div3); 
+
+var span1 = document.createElement("span") ;
+span1.setAttribute("class", "name" );
+span1.innerHTML="BRIAN DUDELY"
+div3.appendChild(span1);
+
+var div4 = document.createElement("div") ;
+div4.setAttribute("class", "add" );
+div4.innerHTML="N.Damen Avenue, Chicago 99999 | 9999 9999 999 999 | hello@kickresume | www.kickresume.com"
+document.body.appendChild(div4);
+
+document.body.appendChild(document.createElement("hr")) ;
+
+var div5 = document.createElement("div") ;
+div5.setAttribute("class", "vertical" );
+document.body.appendChild(div5);
+
+var div6 = document.createElement("div") ;
+div6.setAttribute("class","profile")
+document.body.appendChild(div6);
+
+var img2 = document.createElement("img") ;
+img2.setAttribute("src","golden-star-11530936361kogn1inkrv.png")
+img2.setAttribute("class","star")
+div6.appendChild(img2);
+
+var h1 = document.createElement("h1") ;
+h1.innerHTML="Profile";
+div6.appendChild(h1);
+
+var div7 = document.createElement("div") ;
+div7.setAttribute("class","protext")
+div7.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div6.appendChild(div7);
+
+
+var div8 = document.createElement("div") ;
+div8.setAttribute("class","profile")
+document.body.appendChild(div8);
+
+var img2 = document.createElement("img") ;
+img2.setAttribute("src","golden-star-11530936361kogn1inkrv.png")
+img2.setAttribute("class","star2")
+div8.appendChild(img2);
+
+var h1 = document.createElement("h1") ;
+h1.innerHTML="Skills";
+div8.appendChild(h1);
+
+
+var div9 = document.createElement("div") ;
+div9.setAttribute("class","protext")
+div8.appendChild(div9);
+
+var h1 = document.createElement("b") ;
+div9.setAttribute("class","protext2");
+h1.innerHTML="Technical Skills";
+div9.appendChild(h1);
+
+
+var slide = document.createElement("div") ;
+slide.setAttribute("class","slidecontainer")
+document.body.appendChild(slide);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Javascript";
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="CSS";
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="HTML";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="React";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Redux";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Mongo";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Deployment";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+
+var div11 = document.createElement("div") ;
+div11.setAttribute("class","profile");
+
+document.body.appendChild(div11);
+
+var div12 = document.createElement("div") ;
+div12.setAttribute("class","protext2");
+// h2.innerHTML="Additional Skills";
+div11.appendChild(div12);
+
+var h2 = document.createElement("b") ;
+h2.innerHTML="Additional Skills"
+div12.appendChild(h2);
+
+var slide = document.createElement("div") ;
+slide.setAttribute("class","slidecontainer")
+document.body.appendChild(slide);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Project Management";
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Recruitment";
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Business Developement";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Editing";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+var l1 = document.createElement("label") ;
+l1.setAttribute("class","sname");
+l1.innerHTML="Fund Raising";       
+slide.appendChild(l1);
+
+var i1 = document.createElement("input") ;
+i1.setAttribute("type","range")
+slide.appendChild(i1);
+
+var br = document.createElement("br") ;
+slide.appendChild(br);
+
+
+var div13 = document.createElement("div") ;
+div13.setAttribute("class","profile")
+document.body.appendChild(div13);
+
+var img2 = document.createElement("img") ;
+img2.setAttribute("src","golden-star-11530936361kogn1inkrv.png")
+img2.setAttribute("class","star3")
+div13.appendChild(img2);
+
+var h1 = document.createElement("h1") ;
+h1.innerHTML="Work Experience";
+div13.appendChild(h1);
+
+var span2 = document.createElement("span");
+div13.appendChild(span2);
+
+var b = document.createElement("b");
+b.innerHTML="Event Manager";
+span2.appendChild(b);
+
+var div15 = document.createElement("div") ;
+div15.setAttribute("class","protext");
+// div15.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div13.appendChild(div15);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+
+
+
+
+
+
+
+var side=document.createElement("div");
+side.setAttribute("class","l");
+document.body.appendChild(side);
+
+
+
+
+var div13 = document.createElement("div") ;
+div13.setAttribute("class","profile")
+side.appendChild(div13);
+
+var img2 = document.createElement("img") ;
+img2.setAttribute("src","golden-star-11530936361kogn1inkrv.png")
+img2.setAttribute("class","star4")
+div13.appendChild(img2);
+
+var h1 = document.createElement("h1") ;
+h1.innerHTML="Work Experience";
+div13.appendChild(h1);
+
+var span2 = document.createElement("span");
+div13.appendChild(span2);
+
+var b = document.createElement("b");
+b.innerHTML="Community Relations Manager";
+span2.appendChild(b);
+
+
+
+var div15 = document.createElement("div") ;
+div15.setAttribute("class","protext");
+// div15.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div13.appendChild(div15);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+
+
+
+
+var div13 = document.createElement("div") ;
+div13.setAttribute("class","profile")
+side.appendChild(div13);
+
+var img2 = document.createElement("img") ;
+img2.setAttribute("src","golden-star-11530936361kogn1inkrv.png")
+img2.setAttribute("class","star4")
+div13.appendChild(img2);
+
+var h1 = document.createElement("h1") ;
+h1.innerHTML="Education";
+div13.appendChild(h1);
+
+var span2 = document.createElement("span");
+span2.innerHTML="Engineering Immersion Program";
+div13.appendChild(span2);
+
+
+var div15 = document.createElement("div") ;
+div15.setAttribute("class","protext");
+// div15.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div13.appendChild(div15);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+var l2=document.createElement("li");
+l2.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo inventore veritatis magni,expedita quidem quo, quae quas provident adipisci sit quod praesentium saepe, tempore reiciendis ea ducimus eius dolorum.";
+div15.appendChild(l2);
+
